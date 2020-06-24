6c9cd7544806815eb195387b6f27bc21ca59d2a5
August Aldred <augustaldred@bpp.com>
2020-06-24 10:09:06 +0100

    completed level 100

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const container = document.createElement("DIV");
+  container.classList.add("wrapper");
+  rootElem.appendChild(container);
+
+  episodeList.forEach(episode => {
+    const episodeWrapper = document.createElement("DIV");
+    episodeWrapper.classList.add("episode_wrapper");
+
+    const episodeInfo = `
+    <div role="presentation none" class="info">
+    <p class="title">${episode.name}</p>
+    <p class="season_ep">S0${episode.season}E0${episode.number}</p>
+    <img src="${episode.image.medium}" class="episode_img" alt="screencap from the episode"/>
+    ${episode.summary}
+    </div>`
+
+    episodeWrapper.innerHTML = episodeInfo;
+
+    container.appendChild(episodeWrapper);
+  })
 }
+
 
 window.onload = setup;
