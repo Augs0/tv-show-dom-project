25a66d6e1295a7bb76620864ebd8618e3079df80
August Aldred <AugustAldred@bpp.com>
2020-06-26 14:06:51 +0100

    completed level 200

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function makePageForEpisodes(episodeList) {
 
     const episodeInfo = `
     <div role="presentation none" class="info">
-    <p class="title">${episode.name}</p>
-    <p class="season_ep">S0${episode.season}E0${episode.number}</p>
+    <h2 class="title">${episode.name}</h2>
+    <h3 class="season_ep">S0${episode.season}E0${episode.number}</h3>
     <img src="${episode.image.medium}" class="episode_img" alt="screencap from the episode"/>
     ${episode.summary}
     </div>`
@@ -27,6 +27,35 @@ function makePageForEpisodes(episodeList) {
     container.appendChild(episodeWrapper);
   })
 }
+
+// 200 - Search functionality
+const listEps = getAllEpisodes();
+
+const input = document.getElementById('searchBar');
+const resNum = document.getElementById('res_num');
+
+input.addEventListener('input', () => {
+  searchEpisodes(listEps)
+}, false)
+
+function searchEpisodes(episodeList) {
+  let formattedInput = input.value.toLowerCase();
+
+  let searchedEps = episodeList.filter((ep) => {
+    return ep.name.toLowerCase().indexOf(formattedInput) > -1 || ep.summary.toLowerCase().indexOf(formattedInput) > -1
+  })
+
+  resNum.innerHTML = searchedEps.length + ' of ' + episodeList.length + 'episodes'
+
+  if (searchedEps.length >= 1) {
+    const wrapper = document.getElementsByClassName('wrapper')[0];
+    wrapper.parentNode.removeChild(wrapper);
+    makePageForEpisodes(searchedEps);
+  } else {
+    return
+  }
+}
+
 
 
 window.onload = setup;
